eeb197997478dc911464caff2674a0ebb6e183cb
Luís Gabriel Gomes da Silva <luis_gabriell1@hotmail.com>
2024-10-18 01:06:14 -0300

    garantindo que o migration vai criar e inserir dados

--- src/db/migrations/1729197517526-clientes-table.ts
+++ src/db/migrations/1729197517526-clientes-table.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class ClientesTable1729197517525 implements MigrationInterface {
+export class ClientesTable1729197517526 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
       await queryRunner.query(
         `CREATE TABLE "clientes" (
